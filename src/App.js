c2fee1846d9069a571dccd1e2a81d4dcdff09995
Bushful <dmytro.dzhuriak@predict.systems>
2021-10-13 17:38:33 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
-
 import React from 'react';
 import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
- 
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import Home from './components/Home';
